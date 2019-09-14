970cf0300ba85149de74f75b08c59d4d8a5ffe4e
mnreyes1 <mnreyes1@uc.cl>
2019-09-14 11:55:35 -0300

    added netrc for git login

--- desktopUbuntu.sh
+++ desktopUbuntu.sh
@@ -109,11 +109,6 @@ wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install sublime-text
-## light para cambiar brillo (first download the release)
-# tar xf light-x.yy.tar.gz
-# cd light-x.yy/
-# ./configure && make
-# sudo make install
 echo ""
 echo ""
 echo ""
@@ -136,3 +131,11 @@ echo ""
 cd ~
 cp ~/Repos/desktop/pull_repos.sh ~/pull_repos.sh
 sudo chmod u+x pull_repos.sh
+echo "[COPYING NETRC TO GIT]"
+cp ~/Repos/desktop/.netrc ~/.netrc
+# you have to edit this files later and install the following
+## light to change screen brightness (first download the release)
+# tar xf light-x.yy.tar.gz
+# cd light-x.yy/
+# ./configure && make
+# sudo make install
